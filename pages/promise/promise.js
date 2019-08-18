f4633362eb2de02bd0f97d8d62a897e6e197806d
trevorlink <lvpidadiao@gmail.com>
2019-08-18 23:52:25 +0800

    first push

---
+++ pages/promise/promise.js
@@ -0,0 +1,15 @@
+//logs.js
+
+Page({
+  data: {
+    logs: [],
+    img: {
+      imgSrc : "../../static/sunflower.jpg",
+      name : "向日葵"
+    }
+  },
+  onLoad: function () {
+    this.setData({
+    })
+  }
+})
